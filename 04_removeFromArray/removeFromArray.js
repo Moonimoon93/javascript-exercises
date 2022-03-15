a916dab76cf62f8c41ba4fed5460ad648e5d62f6
Moonimoon93 <harrygularski@gmail.com>
2022-03-15 22:48:51 +0900

    Finished 04. Learned Array.from(arguments). Practiced indexOf and splice method for array.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array) {
+    let removeThese = Array.from(arguments);
+    for (let i = 1; i < removeThese.length; i++) {
+        if (array.indexOf(removeThese[i]) != -1) {
+            let location = array.indexOf(removeThese[i])
+            array.splice(location, 1);
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
