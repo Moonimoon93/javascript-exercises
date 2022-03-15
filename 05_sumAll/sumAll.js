f605d082ed6dba5c39d4f74e2329e765b570132a
Moonimoon93 <harrygularski@gmail.com>
2022-03-15 23:17:28 +0900

    Finished 05. Learned typeof operator and a couple methods to validate for correct parameters for functions.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,29 @@
-const sumAll = function() {
+const sumAll = function(fromHere, toThere) {
+    let sum = 0;
+    //Validating whether the correcct type of parameters is provided or not.
+    if (fromHere < 0 || toThere < 0) {
+        return "ERROR"
+    } else if (typeof fromHere != "number" || typeof toThere != "number") {
+        return "ERROR"
+    }
 
+    if (fromHere < toThere) {
+        let howMany = toThere - fromHere + 1;
+        for (let i = 0; i < howMany; i++) {
+            sum += fromHere;
+            fromHere++;
+        }
+    } else if (fromHere > toThere) {
+        let howMany = fromHere - toThere + 1;
+        for (let i = 0; i < howMany; i++) {
+            sum += toThere;
+            toThere++;
+        }
+    } else if (fromHere === toThere) {
+        sum = fromHere;
+        return sum
+    }
+    return sum;
 };
 
 // Do not edit below this line
