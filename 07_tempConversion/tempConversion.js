cd033f7e8378983ee550f92c45e3e8f4bca5ce64
Moonimoon93 <harrygularski@gmail.com>
2022-03-16 12:25:11 +0900

    Finished 07.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,22 @@
-const ftoc = function() {
+const ftoc = function(degreeF) {
+    let convertedDegree = ((degreeF - 32) * 5 / 9)
+    if (convertedDegree % 1 != 0) {
+        convertedDegree = Number(convertedDegree.toFixed(1));
+    }
 
+    return convertedDegree;
 };
 
-const ctof = function() {
-
+const ctof = function(degreeC) {
+    let convertedDegree = (degreeC * 9 / 5 + 32)
+    if (convertedDegree % 1 != 0) {
+        convertedDegree = Number(convertedDegree.toFixed(1));
+    }
+    return convertedDegree;
 };
 
 // Do not edit below this line
 module.exports = {
-  ftoc,
-  ctof
+    ftoc,
+    ctof
 };
