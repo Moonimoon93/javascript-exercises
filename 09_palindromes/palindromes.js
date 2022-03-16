ca2375173e6e6d2fa3292abcd8145f59fd6438ed
Moonimoon93 <harrygularski@gmail.com>
2022-03-16 18:30:09 +0900

    Finished 09. Practiced using push(), split(), reverse(), and basic for loop with if else statement. It was a lot of fun.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,31 @@
-const palindromes = function () {
+const palindromes = function(reverseMe) {
+    //Made a function for removing punctuations
+    const removePunc = (phraseArr) => {
+        let temp = [];
+        let collectPunc = [];
+        for (let i = phraseArr.length - 1; i >= 0; i--) {
+            if (phraseArr[i] == "." || phraseArr[i] == "," || phraseArr[i] == "!" || phraseArr[i] == "?" || phraseArr[i] == " ") {
+                collectPunc.push(phraseArr[i]);
+            } else {
+                temp.push(phraseArr[i]);
+            }
+        }
+        return temp;
+    }
+    let original = reverseMe.toLowerCase().split("");
+    let reversed = reverseMe.toLowerCase().split("").reverse()
 
+    // original = removePunc(original);
+    console.log("original:", original, original.length);
+    console.log("reversed:", reversed, reversed.length);
+    original = removePunc(original).join("");
+    reversed = removePunc(reversed).join("");
+
+    if (original === reversed) {
+        return true
+    } else {
+        return false
+    }
 };
 
 // Do not edit below this line
