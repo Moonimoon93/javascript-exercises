8d589fe780d5197d8e146aa6572f182e8e001953
Moonimoon93 <harrygularski@gmail.com>
2022-03-28 10:00:10 +0900

    Exercise 12 is finished. 1. practiced with new Date() and getFullYear() methods. 2. array.prototype.sort() was super useful to figure out ages of given people's bday and death date.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    const oldest = people.sort((a,b)=> {
+        if(a.yearOfDeath === undefined){
+            a.yearOfDeath = (new Date()).getFullYear()
+        }else if(b.yearOfDeath === undefined){
+            b.yearOfDeath = (new Date()).getFullYear();
+        }
+        return (b.yearOfDeath - b.yearOfBirth) - (a.yearOfDeath - a.yearOfBirth)
+    })
+    console.log(people);
+    return oldest[0];
 };
 
 // Do not edit below this line
